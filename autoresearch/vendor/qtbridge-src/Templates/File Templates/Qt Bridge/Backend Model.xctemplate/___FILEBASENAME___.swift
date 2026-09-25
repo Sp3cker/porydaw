1599227cbed9a6dda599ddb6a8c1b54bce9cab56
Sp3cker <speker97@protonmail.com>
2026-09-25 11:17:52 -0500

    Remove autoresearch harness: experiment moved to feature/autoresearch worktree

--- autoresearch/vendor/qtbridge-src/Templates/File Templates/Qt Bridge/Backend Model.xctemplate/___FILEBASENAME___.swift
+++
@@ -1,9 +0,0 @@
-import QtBridge
-import Foundation
-
-@QtBridgeable
-public class ___VARIABLE_productName:identifier___ {
-    public func greeting() {
-        print("Hello from ___VARIABLE_productName:identifier___!")
-    }
-}
